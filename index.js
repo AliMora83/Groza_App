6bf6d97a78d93621bf8ff23449a1f350ddc52b72
Daurius Swarts <ali@openmindi.co.za>
2023-07-06 16:22:17 +0200

    [BUG] Made Cake visible

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://playground-e4fb8-default-rtdb.firebaseio.com/"
-}
+  databaseURL:
+    "https://playground-e4fb8-default-rtdb.firebaseio.com/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
-const shoppingListEl = document.getElementById('shopping-list')
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(
+  database,
+  "shoppingList"
+);
+const shoppingListEl = document.getElementById(
+  "shopping-list"
+);
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById(
+  "input-field"
+);
+const addButtonEl =
+  document.getElementById("add-button");
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+addButtonEl.addEventListener(
+  "click",
+  function () {
+    let inputValue = inputFieldEl.value;
+
+    push(shoppingListInDB, inputValue);
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
     // Challenge: Append a new <li> with text content inputValue to the 'shopping-list' <ul>
-})
+  }
+);
