fc388b143b0d3ba84259d4b6de7f37dc06bbc6dd
Daurius Swarts <ali@openmindi.co.za>
2023-07-07 15:16:46 +0200

    [BUG] Ddraft 1 Complete

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL:
-    "https://playground-e4fb8-default-rtdb.firebaseio.com/",
+    "https://playground-e4fb8-default-rtdb.firebaseio.com//",
 };
 
 const app = initializeApp(appSettings);
@@ -16,30 +18,72 @@ const shoppingListInDB = ref(
   database,
   "shoppingList"
 );
-const shoppingListEl = document.getElementById(
-  "shopping-list"
-);
 
 const inputFieldEl = document.getElementById(
   "input-field"
 );
 const addButtonEl =
   document.getElementById("add-button");
+const shoppingListEl = document.getElementById(
+  "shopping-list"
+);
 
 addButtonEl.addEventListener(
   "click",
   function () {
     let inputValue = inputFieldEl.value;
+
     push(shoppingListInDB, inputValue);
-    clearInputEl();
-    appendShoppingEl(inputValue);
+
+    clearInputFieldEl();
   }
 );
 
-function clearInputEl() {
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(
+      snapshot.val()
+    );
+
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML =
+      "No items here... yet";
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
   inputFieldEl.value = "";
 }
 
-function appendShoppingEl(itemValue) {
-  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+function appendItemToShoppingListEl(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(
+      database,
+      `shoppingList/${itemID}`
+    );
+
+    remove(exactLocationOfItemInDB);
+  });
+
+  shoppingListEl.append(newEl);
 }
